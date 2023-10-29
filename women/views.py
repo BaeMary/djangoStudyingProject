d26d44e4bd7a8e3adf707015f948eeb39b26c498
Artem Davtyan <artem_davtyan7@mail.ru>
2023-10-29 22:03:46 +0300

    Lesson 5.6 updates
    * Django Debug Toolbar is connected
    * SQL requests optimizations via selected_related

--- women/views.py
+++ women/views.py
@@ -14,15 +14,9 @@ menu = [
     {'title': 'Войти', 'url_name': 'login'},
 ]
 
-data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Боиграфия Анджелины Джоли', 'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Боиграфия Марго Робби', 'is_published': False},
-    {'id': 3, 'title': 'Джулия Роберт', 'content': 'Боиграфия Джулии Робертс', 'is_published': True},
-]
-
 
 def index(request):
-    posts = Women.published.all()
+    posts = Women.published.all().select_related('cat')
     data = {
         'title': 'Главная страница',
         'menu': menu,
@@ -61,7 +55,7 @@ def show_post(request, post_slug):
 
 def show_category(request, cat_slug):
     category = get_object_or_404(Category, slug=cat_slug)
-    posts = Women.published.filter(cat_id=category.pk)
+    posts = Women.published.filter(cat_id=category.pk).select_related('cat')
     data = {
         'title': f'Рубрика {category.name}',
         'menu': menu,
@@ -77,7 +71,7 @@ def page_not_found(request, exception):
 
 def show_tag_postlist(request, tag_slug):
     tag = get_object_or_404(TagPost, slug=tag_slug)
-    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED)
+    posts = tag.tags.filter(is_published=Women.Status.PUBLISHED).select_related('cat')
     data = {
         'title': f'Тэг: {tag.tag}',
         'menu': menu,

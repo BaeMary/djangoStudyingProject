392546b6ff99fe2867a79255497b0898906b9700
Artem Davtyan <artem_davtyan7@mail.ru>
2023-11-09 22:04:35 +0300

    Lesson 9.5 updates
    * login_required decorator and LoginRequiredMixin class discussed
    * Stuff above added

--- women/views.py
+++ women/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
@@ -21,6 +23,7 @@ class WomenHome(DataMixin, ListView):
         return Women.published.all().select_related('cat')
 
 
+@login_required
 def about(request):
     contact_list = Women.published.all()
     paginator = Paginator(contact_list, 3)
@@ -29,10 +32,15 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'page_obj': page_obj})
 
 
-class AddPage(DataMixin, CreateView):
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
     form_class = AddPostForm
     template_name = 'women/addpage.html'
     title_page = 'Добавление статьи'
+
+    def form_valid(self, form):
+        w = form.save(commit=False)
+        w.author = self.request.user
+        return super().form_valid(form)
 
 
 class UpdatePage(DataMixin, UpdateView):

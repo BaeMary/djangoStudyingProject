8fbc4628c0d0e0b8ddf130872174adfaf52aeb8a
Artem Davtyan <artem_davtyan7@mail.ru>
2023-11-13 18:48:42 +0300

    Lesson 10.3 updates
    * GitHub authentication set up

--- sitewomen/settings.py
+++ sitewomen/settings.py
@@ -140,7 +140,7 @@ LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'users:login'
 
 AUTHENTICATION_BACKENDS = [
-    'social_core.backends.github.GithubOAuth2'
+    'social_core.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
     'users.authentication.EmailAuthBackend',
 ]
@@ -160,3 +160,19 @@ EMAIL_ADMIN = EMAIL_HOST_USER
 AUTH_USER_MODEL = 'users.User'
 
 DEFAULT_USER_IMAGE = MEDIA_URL + 'users/default.png'
+
+SOCIAL_AUTH_GITHUB_KEY = 'be8f82eb699d4907bcbf'
+SOCIAL_AUTH_GITHUB_SECRET = '1d66690f069f2e260db3400c9af7341590f3d86f'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'users.pipeline.new_users_handler',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
